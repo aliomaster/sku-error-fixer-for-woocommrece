be48387829923034990dcc40411386938cf70243
almazka <almazka@flylady.su>
2016-03-26 21:11:58 +0600

    edited settings page

--- templates/main_settings.php
+++ templates/main_settings.php
@@ -1,7 +1,57 @@
 <?php 
-if (isset($_POST[VKPB_USER_ID_OPTION]) && isset($_POST[VKPB_CLIENT_ID_OPTION]) && isset($_POST[VKPB_ACCESS_TOKEN_OPTION])) {
-	update_option(VKPB_USER_ID_OPTION, $_POST[VKPB_USER_ID_OPTION]);
-	update_option(VKPB_CLIENT_ID_OPTION, $_POST[VKPB_CLIENT_ID_OPTION]);
-	update_option(VKPB_ACCESS_TOKEN_OPTION, $_POST[VKPB_ACCESS_TOKEN_OPTION]);
-	echo "<div class='updated'><p>Настройки сохранены</p></div>";
+if ( isset( $_POST[SWS_VAR_CLEANER_AUTO]) ) {
+	update_option( SWS_VAR_CLEANER_AUTO, $_POST[SWS_VAR_CLEANER_AUTO] );
+	echo "<div class='updated'><p>" . _e( 'Settings updated' ) . "</p></div>";
 }
+?>
+<div class="wrap">
+	<h2 class="dashicons-before dashicons-admin-generic options_icon">
+		<?php echo SWS_VAR_CLEANER_PLUGIN_NAME; ?>
+	</h2>
+	<form method="POST" action="options" enctype="multipart/form-data">
+	<?php //settings_fields( 'theme_option_group' ); ?>
+	<table class="form-table wc_sku_cleaner">
+		<tr>
+			<th scope="row">
+				<?php _e( 'Search old product variations' ); ?>
+				<p class="description">
+					<?php _e( 'Click Start search button to start the search of old variations.' ); ?>
+				</p>
+			</th>
+			<td>
+				<button class="button button-primary"><?php _e( 'Start search old variations' ); ?></button>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">
+				<?php _e( 'Bulk delete' ); ?>
+				<p class="description">
+					<?php _e( 'Simultaneous removal of all the old variables or cleaning not relevant SKU numbers.' ); ?>
+				</p>
+			</th>
+			<td>
+				<button class="button button-primary"><?php _e( 'Clean SKU fields of old variations' ); ?></button>
+				<hr class="cleaner_divider">
+				<button class="button button-primary"><?php _e( 'Delete the traces of the old variations fully' ); ?></button>
+				<p class="description">
+					<?php _e( 'This operation cannot be undone. Please backup your database if you are unsure.' ); ?>
+				</p>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">
+				<?php _e( 'Automatically' ); ?>
+				<p class="description">
+					<?php _e( 'When I change the product type to Simple from Variable:' ); ?>
+				</p>
+			</th>
+			<td class="auto_labels">
+				<p><label for="auto_clean0"><input type="radio" name="auto_clean" value="default" id="auto_clean0"> <?php _e( 'Default (not clean)' ); ?></label></p>
+				<p><label for="auto_clean1"><input type="radio" name="auto_clean" value="auto_del_fully" id="auto_clean1"> <?php _e( 'Automatically delete old variations' ); ?></label></p>
+				<p><label for="auto_clean2"><input type="radio" name="auto_clean" value="auto_del_sku" id="auto_clean2"> <?php _e( 'Automatically clean old skus of variables' ); ?></label></p>
+			</td>
+		</tr>
+	</table>
+	<?php submit_button(); ?>
+	</form>
+</div>
--- woocommerce-sku-variations-cleaner-sws.php
+++ woocommerce-sku-variations-cleaner-sws.php
@@ -1,14 +1,25 @@
 <?php
 /**
 * Plugin Name: Woocommerce SKU Variations Cleaner SWS
-* Description: ...
+* Description: Removal old product SKUs or configure automatic deletion when changing the product type.
 * Version: 1.0
-* Author: Alio Master
+* Author: Alio Master (SWS - SpaceWorkSpace)
 * Author URI: aliowebdeveloper@gmail.com
+*
+* Text Domain: wordpress-plugin-template
+* Domain Path: /lang/
+*
+* @package WordPress
+* @author Alio Master (SWS - SpaceWorkSpace)
+* @since 1.0.0
 */
 
-define( SWS_PLUGIN_NAME, 'Woocommerce SKU Variations Cleaner SWS' );
-define( SWS_TEMPLATE_PATH, plugin_dir_path( __FILE__ ) . "templates/" );
+define( 'SWS_VAR_CLEANER_PLUGIN_NAME', 'Woocommerce SKU Variations Cleaner SWS' );
+define( 'SWS_VAR_CLEANER_PLUGIN_URL', __FILE__ );
+define( 'SWS_VAR_CLEANER_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'SWS_VAR_CLEANER_TEMPLATE_PATH', plugin_dir_path( __FILE__ ) . "templates/" );
+define( 'SWS_VAR_CLEANER_AUTO', 'auto_clean' );
+
 /* задать константы опций, чтобы сохранялись в бд */
 
 /**
@@ -25,19 +36,63 @@ class SKU_Variations_Cleaner {
 	 * @return void
 	 */
 	function sku_variations_cleaner() {
-		add_action( 'admin_menu', array( &$this, 'setting_page_menu' ), 99 );
+		add_action( 'admin_menu', array( $this, 'setting_page_menu' ), 99 );
 		//add_action( 'admin_init', array( &$this, 'get_title' ) );
 		//add_filter( 'media_upload_tabs', array( &$this, 'create_new_tab') );
 		//add_action( 'media_buttons', array( &$this, 'context'), 11 );
 		//add_filter( 'media_upload_uploadzip', array( &$this, 'media_upload_uploadzip') );
+
+		// Load admin JS & CSS
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ), 10, 1 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_styles' ), 10, 1 );
+
+		add_filter( "plugin_action_links", array( $this, 'sku_variations_cleaner_plugin_action_links' ), 10, 2 );
+
 	}
+
+	/**
+	 * Load admin CSS.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function admin_enqueue_styles () {
+		wp_register_style( 'css-sku-vars-cleaner-admin', plugins_url( 'css/sku-vars-cleaner-admin.css', __FILE__ ) );
+		wp_enqueue_style( 'css-sku-vars-cleaner-admin' );
+	}
+
+	/**
+	 * Load admin Javascript.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function admin_enqueue_scripts () {
+		wp_register_script( 'js-sku-vars-cleaner-admin', plugins_url( 'js/sku-vars-cleaner-admin.js', __FILE__ ), array( 'jquery' ) );
+		wp_enqueue_script( 'js-sku-vars-cleaner-admin' );
+	}
+
+	/**
+	 * Add settings link to plugin list table
+	 * @param  array $links Existing links
+	 * @return array 		Modified links
+	 */
+	function sku_variations_cleaner_plugin_action_links( $links, $file ) {
+		if ( $file == plugin_basename( SWS_VAR_CLEANER_PLUGIN_PATH . 'woocommerce-sku-variations-cleaner-sws.php' ) ) {
+			$settings_link = '<a href="admin.php?page=sku_variations_cleaner">'
+			. _e( 'Plugin Settings' ) . '</a>';
+			array_unshift( $links, $settings_link );
+		}
+		return $links;
+	}
+
 	/**
 	 * Create admin pages in menu
 	 *
 	 * @return void
 	 */
 	function setting_page_menu() {
-		$page = add_submenu_page( 'woocommerce', SWS_PLUGIN_NAME, 'Clear Variations', 'manage_options', 'sku_variations_cleaner', array( &$this, 'sku_variations_cleaner_page' ) );
+		$page = add_submenu_page( 'woocommerce', SWS_VAR_CLEANER_PLUGIN_NAME, 'Clear Variations', 'manage_options', 'sku_variations_cleaner', array( $this, 'sku_variations_cleaner_page' ) );
 	}
 
 	/**
@@ -47,7 +102,7 @@ class SKU_Variations_Cleaner {
 	 * @return array $tabs Modified media tabs
 	 */
 	function sku_variations_cleaner_page() {
-		include SWS_TEMPLATE_PATH . "main_settings.php";
+		include SWS_VAR_CLEANER_TEMPLATE_PATH . "main_settings.php";
 	}
 	/**
 	 * Move unzipped content from temp folder to media library
